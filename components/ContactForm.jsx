337dc89edc1dddf18c1b1455b98648ab86571dd5
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2022-03-22 20:13:30 -0300

    update contact info

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -19,7 +19,7 @@ const ContactForm = () => {
                   clipRule='evenodd'
                 ></path>
               </svg>
-              <span>Porto Feliz/SP</span>
+              <span>Porto Feliz - SP</span>
             </p>
             <p className='flex items-center'>
               <svg
@@ -42,7 +42,7 @@ const ContactForm = () => {
                 <path d='M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z'></path>
                 <path d='M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z'></path>
               </svg>
-              <span>contact@business.com</span>
+              <span>ads_consultoria@outlook.com</span>
             </p>
           </div>
         </div>
