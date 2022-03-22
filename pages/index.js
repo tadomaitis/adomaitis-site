0cb2d8840ae8babac9bdea06359f944727b1e129
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2022-03-22 19:30:27 -0300

    update page title

--- pages/index.js
+++ pages/index.js
@@ -10,19 +10,22 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Adomaitis consultoria</title>
-        <meta name='description' content='Descubra como revolucionar o seu negócio com tecnologia e transformação digital'/>
-        <link rel="icon" href="/favicon.svg" />
+        <title>ADS Consultoria</title>
+        <meta
+          name='description'
+          content='Descubra como revolucionar o seu negócio com tecnologia e transformação digital'
+        />
+        <link rel='icon' href='/favicon.svg' />
       </Head>
       <Navbar />
       <Header />
-      <div className="divider w-10/12 mx-auto mt-8"></div> 
+      <div className='divider w-10/12 mx-auto mt-8'></div>
       <AboutUs />
-      <div className="divider w-10/12 mx-auto"></div> 
+      <div className='divider w-10/12 mx-auto'></div>
       <Services />
-      <div className="divider w-10/12 mx-auto"></div> 
+      <div className='divider w-10/12 mx-auto'></div>
       <ContactForm />
-      <div className="divider w-10/12 mx-auto"></div>       
+      <div className='divider w-10/12 mx-auto'></div>
       <Footer />
     </>
   )
